69313e012bc2767268a4ec5eed4e28e35a89e756
Mert Yılmaz <b202102029@subu.edu.tr>
2021-10-23 22:55:24 +0300

    Kamp ödevi tekrarı

--- src/app.js
+++ src/app.js
@@ -1,27 +1,61 @@
-console.log("Merhaba Kodlama.io");
+//var,let,const
 
-//JS type safe değildir.
-let dolarBugun = 9.30
+let sayi1 = 10;
+sayi1 = "Engin Demiroğ"
+let student = {id:1, name:"Engin"}
+//console.log(student)
 
-let dolarDun  = 9.20
-
+function save(puan=10,ogrenci) 
 {
-    let dolarDun = 9.10
+    console.log(ogrenci.name + " : " + puan)
+}
+//save(undefined,student);
+
+let students = ["Mert","Başar","İlker","Melih"]
+
+//console.log(students)
+
+let students2 = [student,{id:2, name:"Yavuz"},"İstanbul",{city:"İzmir"}]
+//console.log(students2)
+
+//REST
+//PARAMS (C#)
+//varArgs(JAVA)
+let showProducts = function (id,...products) 
+{
+    console.log(id)
+    console.log(products[0])    
 }
 
-console.log(dolarDun);
+//console.log(typeof showProducts)
+//showProducts(10,["Elma","Armut","Karpuz"])
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"] 
+//SPREAD
+let points = [1,2,3,4,50,4,60,14]
+//console.log(...points)
+//console.log(Math.max(...points))
+//console.log(..."ABC","D",..."EFG","H")
 
-console.log("<ul>")
+//DESTRUCTURING
+let populations = [10000,20000,30000,[40000,100000]]
+let [small,medium,high,[veryHigh,max]] = populations
+// console.log(small)
+// console.log(medium)
+// console.log(high)
+// console.log(veryHigh)
+// console.log(max)
 
-for (let i = 0; i < konutKredileri.length; i++) {
-    console.log("<li>" + konutKredileri[i] + "</li>") 
+function someFunction([small1],number) 
+{
+    console.log(small1)
 }
 
-console.log("</ul>")
+//someFunction(populations)
 
-console.log(konutKredileri)
+let category = {id:1, name:"İçecek"}
+console.log(category.id)
+console.log(category.name) //or console.log(category["name"])
+
+let {id,name} = category
+console.log(id)
+console.log(name)
